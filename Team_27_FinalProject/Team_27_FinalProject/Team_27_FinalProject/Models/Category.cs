19570cd67ca3be917e5842c20f56576fea26341a
chautrinh26 <89857400+chautrinh26@users.noreply.github.com>
2021-11-21 21:56:15 -0600

    Add Review + Category Models

---
+++ Team_27_FinalProject/Team_27_FinalProject/Team_27_FinalProject/Models/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Team_27_FinalProject.Models
+{
+    public class Category
+    {
+        public Int32 CategoryID { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public String Name { get; set; }
+
+        //a category will include many properties
+        public List<Property> Properties { get; set; }
+
+        public Category()
+        {
+            if (Properties == null)
+            {
+                Properties = new List<Property>();
+            }
+        }
+    }
+}
