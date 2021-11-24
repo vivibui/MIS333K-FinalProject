bfbab82ccc30cd4ec929b07166581c59d426ef43
vivibui <vivianbui9889@gmail.com>
2021-11-24 12:50:32 -0600

    Add null preference AppUser

--- Team_27_FinalProject/Team_27_FinalProject/Models/AppUser.cs
+++ Team_27_FinalProject/Team_27_FinalProject/Models/AppUser.cs
@@ -1,30 +1,41 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Team_27_FinalProject.Models
 {
     public class AppUser:IdentityUser
     {
         //IsActive (Y/N)
+        [Display(Name = "Is Active? (Y/N)")]
         public Boolean IsActive { get; set; }
 
         //First Name
+        [Display(Name = "First Name:")]
         public String FirstName { get; set; }
 
-        //Last Name
-        public String LastName { get; set; }
-
         //Middle Name
+        [Display(Name = "Middle Initial:")]
         public String MI { get; set; }
 
-        //Birthday 
+        //Last Name
+        [Display(Name = "Last Name:")]
+        public String LastName { get; set; }
+
+        //Birthday
+        [Display(Name = "Birthday:")]
         public DateTime Birthday { get; set; }
 
         //Address
+        [Display(Name = "Address:")]
         public String Address { get; set; }
 
-        //Zipcode 
+        //Zipcode
+        [Display(Name = "Zipcode:")]
         public Int32 ZipCode { get; set; }
 
 
@@ -32,6 +43,24 @@ namespace Team_27_FinalProject.Models
         public List<Order> Orders { get; set; }
         public List<Property> Properties { get; set; }
         public List<Review> Reviews { get; set; }
+
+
+        //--------------------PREVENT NULL PREFERENCE
+        public AppUser()
+        {
+            if (Orders == null)
+            {
+                Orders = new List<Order>();
+            }
+            if (Properties == null)
+            {
+                Properties = new List<Property>();
+            }
+            if (Reviews == null)
+            {
+                Reviews = new List<Review>();
+            }
+        }
 
     }
 }
