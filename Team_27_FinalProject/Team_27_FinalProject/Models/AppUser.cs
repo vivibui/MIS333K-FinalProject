7affad4e4f374d8ae02e6b35216d01e37a329225
vivibui <vivianbui9889@gmail.com>
2021-11-24 13:11:01 -0600

    Edited AppUser, Review, and Identity
    1. AppUser model: Add nav properties and display name
    2. Review model: Change name, edit datatype, add error message
    3. Identity:  Change accordingly to AppUser

--- Team_27_FinalProject/Team_27_FinalProject/Models/AppUser.cs
+++ Team_27_FinalProject/Team_27_FinalProject/Models/AppUser.cs
@@ -1,41 +1,64 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Team_27_FinalProject.Models
 {
     public class AppUser:IdentityUser
     {
         //IsActive (Y/N)
-        public Boolean IsActive { get; set; }
+        [Display(Name = "Is Active? (Y/N)")]
+        public Boolean? IsActive { get; set; }
 
         //First Name
+        [Display(Name = "First Name:")]
         public String FirstName { get; set; }
 
         //Last Name
+        [Display(Name = "Last Name:")]
         public String LastName { get; set; }
 
         //Middle Name
+        [Display(Name = "Middle Name:")]
         public String MI { get; set; }
 
-        //Birthday 
+        //Birthday
+        [Display(Name = "Birthday:")]
         public DateTime Birthday { get; set; }
 
-        //Street
-        public String Street { get; set; }
+        //Address
+        [Display(Name = "Address:")]
+        public String Address { get; set; }
 
-        //City
-        public String City { get; set; }
-
-        //State
-        public String State { get; set; }
-
-        //Zipcode 
-        public Int32 Zip { get; set; }
+        //Zipcode
+        [Display(Name = "Zip Code:")]
+        public Int32 ZipCode { get; set; }
 
 
         //--------------------NAVIGATIONAL PROPERTIES
         public List<Order> Orders { get; set; }
+        public List<Property> Properties { get; set; }
+        public List<Review> Reviews { get; set; }
 
+
+        //--------------------PREVENT NULL PREFERENCE
+        public AppUser()
+        {
+            if (Orders == null)
+            {
+                Orders = new List<Order>();
+            }
+            if (Properties == null)
+            {
+                Properties = new List<Property>();
+            }
+            if (Reviews == null)
+            {
+                Reviews = new List<Review>();
+            }
+        }
     }
 }
