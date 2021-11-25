702999edf61cdf1ad8c13562bef6550fed652625
vivibui <vivianbui9889@gmail.com>
2021-11-25 12:14:45 -0600

    Bug fixed for Create Admin Account
    1. Bug fixed for Create Admin Account
    2. Edit Views/RoleAdmin/Edit

--- Team_27_FinalProject/Team_27_FinalProject/Controllers/ReviewsController.cs
+++ Team_27_FinalProject/Team_27_FinalProject/Controllers/ReviewsController.cs
@@ -12,8 +12,6 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Team_27_FinalProject.Controllers
 {
-    //Only logged-in users can access registrations
-    [Authorize]
 
     public class ReviewsController : Controller
     {
@@ -86,6 +84,7 @@ namespace Team_27_FinalProject.Controllers
             return View(review);
         }
 
+
         // POST: Reviews/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -120,6 +119,7 @@ namespace Team_27_FinalProject.Controllers
             }
             return View(review);
         }
+
 
         // GET: Reviews/Delete/5
         public async Task<IActionResult> Delete(int? id)
